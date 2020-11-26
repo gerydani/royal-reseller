fe429f4431eb5446ff1835e14a1dc260eed98283
idzarandalan <id71cacacacaca@gmail.com>
2020-11-26 17:39:48 +0700

    commit alanw

--- resources/views/user/inputbarang.blade.php
+++ resources/views/user/inputbarang.blade.php
@@ -1,41 +1,25 @@
+@extends('layout.main')
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Adminto - Responsive Admin Dashboard Template</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-        <meta content="Coderthemes" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-        <!-- App favicon -->
+@section('icon')
         <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+@endsection
 
-        <!-- App css -->
-        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" />
+@section('css')
         <link href="https://fonts.googleapis.com/css?family=Lato:400,600,700" rel="stylesheet" />
         <link href="{{ asset('colorlib-search-9/css/main.css') }}" rel="stylesheet" />
+@endsection
 
-        <script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
-        <script src="{{ asset('colorlib-search-9/js/extention/choices.js') }}"></script>
-
-    </head>
-    <body>
-                <!-- Page-Title -->
+@section('judul')
                 <div class="row">
                     <div class="col-sm-12">
                         <h4 class="page-title" style="margin-left: 400px">Input Barang</h4>
                     </div>
                 </div>
-                <!-- end page title end breadcrumb -->
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-xl-6">
+@endsection
+@section('content')
+                <div class="col-md-12">
+                    <div class="col-xl-8">
                         <div class="card-box">
-<!-----------------basic form----------------->
                             <h4 class="header-title m-t-0 m-b-30">Data Pengiriman</h4>
                             <form action="#" data-parsley-validate novalidate>
                                     <div class="form-group">
@@ -112,22 +96,16 @@
                                 </div>
                             </form>
                         </div>
-                    </div><!-- end col -->
-            </div> <!-- end container -->
-        <!-- jQuery  -->
-        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('assets/js/waves.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+                    </div>
+                </div>
+            </div>
+@endsection
 
-        <!-- Validation js (Parsleyjs) -->
+@section('js')
         <script type="text/javascript" src="{{ asset('assets/plugins/parsleyjs/dist/parsley.min.js') }}"></script>
+@endsection
 
-        <!-- App js -->
-        <script src="{{ asset('assets/js/jquery.core.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.app.js') }}"></script>
-
+@section('script-js')
         <script type="text/javascript">
             $(document).ready(function() {
                 $('form').parsley();
@@ -147,6 +125,4 @@
             });
         });
         </script>
-
-    </body>
-</html>
+@endsection
