d4de7637a0c2fd14ea40ec96a87227e5de7a8b39
idzarandalan <id71cacacacaca@gmail.com>
2020-12-03 10:52:55 +0700

    commit alanr

--- routes/web.php
+++ routes/web.php
@@ -30,20 +30,11 @@ Route::get('/toko','TokoController@index')->name('toko');
 Route::get('/registoko','TokoController@create')->name('registoko');
 Route::post('/tambahtoko','TokoController@store');
 
-<<<<<<< HEAD
 // menu produk
 Route::get('/produk','ProdukController@index')->name('produk');
 Route::get('/insertproduk','ProdukController@create')->name('tampro');
 Route::post('/tambahproduk','ProdukController@store');
 Route::delete('/deleteproduk/{id}','ProdukController@destroy')->name('produk.destroy');
-=======
-Route::middleware(['checkUser'])->group(function () {
-    Route::get('/logout','HomeController@logout')->name('Logout');
-    Route::post('/order','OrderController@store');
-});
-
-Route::post('/create','UserController@createaturan')->name('buat');
->>>>>>> c3514738f74d8dba029afba2e47341de126663bc
 
 //menu antrian
 Route::get('/order','OrderController@index')->name('order.index');
