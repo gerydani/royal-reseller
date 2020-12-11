072d39da452a85d9c9b0f9905d226e3da339d42f
IT Royal <gerydaniel22@gmail.com>
2020-12-11 15:40:53 +0700

    mantab

--- resources/views/user/editprofile.blade.php
+++ resources/views/user/editprofile.blade.php
@@ -28,7 +28,7 @@ Index Laporan Keuangan
                     <div class="col-xl-6">
                         <div class="card-box" style="width:201%">
                             <h4 class="header-title m-t-0 m-b-30">Data Tersimpan</h4>
-                            <form action="{{ route('update.profile',$user->id) }}" method="post" data-parsley-validate novalidate>
+                            <form action="{{ route('update.profile',['id'=>session('user_id')]) }}" method="post" data-parsley-validate novalidate>
                                 {{ csrf_field() }}
                                 {{-- <label class="col-4 col-form-label"><span class="text-danger">* required field</span></label> --}}
                                 <div class="form-group">
@@ -64,7 +64,7 @@ Index Laporan Keuangan
                                     <label class="col-4 col-form-label">Password</span></label>
                                     <input type="password" name="password" required
                                            placeholder="Password" class="form-control" id=""
-                                           value="@isset($user->password){{$user->password}}@endisset">
+                                           value=""
                                 </div>
                                 <div class="form-group text-right m-b-0">
                                     <button class="btn btn-primary waves-effect waves-light" type="submit">
